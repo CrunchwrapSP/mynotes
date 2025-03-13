024d2b4b6e4d27697af3ee46e3c020b6426b05cf
Emmett Colsch <ew.colsch@gmail.com>
2025-03-12 19:48:02 -0500

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/views/login_views.dart';
+import 'package:mynotes/views/register_views.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,10 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       home: const HomePage(),
+      routes: {
+        '/login/' : (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -21,31 +26,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState){
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if(user?.emailVerified ?? false){
-                print('you are verified');
-              }
-              else {
-                print('you arent verified');
-              }
+              // final user = FirebaseAuth.instance.currentUser;
+              // print(user);
+              // if(user?.emailVerified ?? false){
+              //   return const Text('done');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
             default: 
-              return const Text('Loading...');
+              return const CircularProgressIndicator();
           }
-          return const Text('done');
         },
-      ),
-    );
+      );
   }
 }
+
