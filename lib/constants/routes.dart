5078d67b884c27395a485e1f77ef191408782044
Emmett Colsch <ew.colsch@gmail.com>
2025-04-29 19:04:33 -0500

    step-25

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,5 +1,1 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/';
-const verifyEmailRoute = '/verify-email/';
 const createorUpdateNoteRoute = '/notes/new-note/';
